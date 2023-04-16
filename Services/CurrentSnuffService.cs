c5b8f7c934634c4c047f9133acfa6d364e058d64
Frans Billqvist <frans.billquist@gmail.com>
2023-04-16 22:12:09 +0200

    Added: NumberOfAvilableSnuffInBox

--- Services/CurrentSnuffService.cs
+++ Services/CurrentSnuffService.cs
@@ -1,40 +1,48 @@
-// using DAL;
-// using DAL.Interfaces;
-// using DAL.Models;
-// using Microsoft.Extensions.Options;
-// using MongoDB.Bson;
-// using MongoDB.Driver;
+using DAL;
+using DAL.Interfaces;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
-// namespace Services;
+namespace Services;
 
-// public class CurrentSnuffService
-// {
-//     private readonly IGenericMongoRepository<CurrentSnuff> _currentSnuffRepository;
+public class CurrentSnuffService
+{
+    private readonly IGenericMongoRepository<CurrentSnuff> _currentSnuffRepository;
 
-//     public CurrentSnuffService(
-//         IOptions<SnuffDatabaseSettings> snuffDatabaseSettings, IGenericMongoRepository<CurrentSnuff> currentSnuffRepository)
-//         {
-//             var mongoClient = new MongoClient(
-//                 snuffDatabaseSettings.Value.ConnectionString);
+    public CurrentSnuffService(
+        IOptions<MongoDbSettings> Settings, IGenericMongoRepository<CurrentSnuff> currentSnuffRepository)
+    {
+        var mongoClient = new MongoClient(
+            Settings.Value.ConnectionString);
 
-//             var mongoDatabase = mongoClient.GetDatabase(
-//                     snuffDatabaseSettings.Value.DatabaseName);
+        var mongoDatabase = mongoClient.GetDatabase(
+                Settings.Value.DatabaseName);
 
-//             _currentSnuffRepository = currentSnuffRepository;
-//         }
+        _currentSnuffRepository = currentSnuffRepository;
+    }
 
-// public async Task<List<CurrentSnuff>> GetAllCurrentSnuffAsync() => 
-//     await _currentSnuffRepository.Find(_ => true).ToListAsync();
+    public async Task<int> NumberOfAvilableSnuffInBox(string boxId)
+    {
+        var req = await _currentSnuffRepository.FindOneAsync(x => x.Id == boxId);
+        var result = req.CurrentAmount;
+        return result;
+    }
 
-// public async Task<CurrentSnuff> GetCurrentSnuffAsync(ObjectId id) =>
-//     await _currentSnuffRepository.Find(x => x.Id == id).FirstOrDefaultAsync();
+    // public async Task<List<CurrentSnuff>> GetAllCurrentSnuffAsync() => 
+    //     await _currentSnuffRepository.Find(_ => true).ToListAsync();
 
-// public async Task CreateCurrentSnuffAsync(CurrentSnuff newCurrentSnuff) =>
-//     await _currentSnuffRepository.InsertOneAsync(newCurrentSnuff);
+    // public async Task<CurrentSnuff> GetCurrentSnuffAsync(ObjectId id) =>
+    //     await _currentSnuffRepository.Find(x => x.Id == id).FirstOrDefaultAsync();
 
-// public async Task UpdateCurrentSnuffAsync(ObjectId id, CurrentSnuff updatedCurrentSnuff) =>
-//     await _currentSnuffRepository.ReplaceOneAsync(x => x.Id == id, updatedCurrentSnuff);
+    // public async Task CreateCurrentSnuffAsync(CurrentSnuff newCurrentSnuff) =>
+    //     await _currentSnuffRepository.InsertOneAsync(newCurrentSnuff);
 
-// public async Task RemoveCurrentSnuffAsync(ObjectId id) =>
-//     await _currentSnuffRepository.DeleteOneAsync(x => x.Id == id);
-// }
+    // public async Task UpdateCurrentSnuffAsync(ObjectId id, CurrentSnuff updatedCurrentSnuff) =>
+    //     await _currentSnuffRepository.ReplaceOneAsync(x => x.Id == id, updatedCurrentSnuff);
+
+    // public async Task RemoveCurrentSnuffAsync(ObjectId id) =>
+    //     await _currentSnuffRepository.DeleteOneAsync(x => x.Id == id);
+}
