296940bd29cd5a4ceebb6db177afaf95340e09e5
Frans Billqvist <frans.billquist@gmail.com>
2023-05-18 13:49:57 +0200

    ch: IProgressionService

--- Services/Interfaces/IProgressionService.cs
+++ Services/Interfaces/IProgressionService.cs
@@ -5,8 +5,9 @@ namespace Services.Interfaces;
 public interface IProgressionService
 {
     Task AddNewProgression(Progression newProgression);
-    Task<Progression> FindUserActiveProgression(string uid);
-    Task UpdateProgressionStateAsync(Progression updatedProgression);
-    Task RemoveProgressionAsync(string uid);
     Task<int> CalculateRemaingSnuff(string uid);
+    Task<Progression> FindUserActiveProgression(string uid);
+    Task RemoveProgressionAsync(string pid);
+    Task UpdateProgressionStateAsync(Progression updatedProgression);
+    Task<Progression> ProgressionHandler(Progression progressionData);
 }
