c7560a157f7086652011149727a1bad9f8ac88d6
Frans Billqvist <frans.billquist@gmail.com>
2024-01-26 04:20:33 +0100

    Add FindUserActiveProgressionDto &  MapProgressionToDto to IProgressionService

--- Services/Interfaces/IProgressionService.cs
+++ Services/Interfaces/IProgressionService.cs
@@ -1,4 +1,5 @@
 using DAL;
+using DAL.Dto;
 
 namespace Services.Interfaces;
 
@@ -7,9 +8,11 @@ public interface IProgressionService
     Task<Progression> AddNewProgression(string uid);
     Task<int> CalculateRemainingSnuff(string uid);
     Task<Progression> FindUserActiveProgression(string uid);
+    Task<ProgressionDto> FindUserActiveProgressionDto(string uid);
     Task RemoveProgressionAsync(string pid);
     Task<Progression> UpdateProgressionStateAsync(Progression updatedProgression);
     Task<Progression> ProgressionHandler(string uid);
     Task<string> WhenIsTheNextDoseAvailable(string uid);
     Task<string> LastConsumedSnuffAtUtc(string uid);
+    ProgressionDto MapProgressionToDto(Progression progression);
 }
