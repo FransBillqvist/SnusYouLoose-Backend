26a583e2daed7d9332c6f059c3d9b4b7eb1ae2d9
Frans Billqvist <frans.billquist@gmail.com>
2024-01-29 01:05:44 +0100

    Add SnuffShopDto class for shop in frontend calls

---
+++ DAL/Dto/SnuffShopDto.cs
@@ -0,0 +1,12 @@
+namespace DAl.Dto;
+
+public class SnuffShopDto 
+{
+    public string Id { get; set; }
+    public string Brand { get; set; }
+    public string Type { get; set; }
+    public decimal Price { get; set; }
+    public int DefaultAmount { get; set; }
+    public string ImageUrl { get; set; }     
+
+}
