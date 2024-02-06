3b1d32dd4df6701a94cf9905042d8757376ff53e
Frans Billqvist <frans.billquist@gmail.com>
2024-02-06 23:35:28 +0100

    Add CreateUserV2Async method to IUserService interface

--- Services/Interfaces/IUserService.cs
+++ Services/Interfaces/IUserService.cs
@@ -3,6 +3,7 @@ namespace Services.Interfaces;
 public interface IUserService
 {
     Task CreateUserAsync(User newUser);
+    Task CreateUserV2Async(User newUser);
     Task<User> GetUserAsync(string id);
     Task RemoveUserAsync(string id);
     Task UpdateUserAsync(User updatedUser);
