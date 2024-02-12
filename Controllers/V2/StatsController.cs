2b1b64d7fe2c06bd6eadd4cfad69fbd6d0a942df
Frans Billqvist <frans.billquist@gmail.com>
2024-02-13 00:56:01 +0100

    Add StatsController with API endpoints

---
+++ Controllers/V2/StatsController.cs
@@ -0,0 +1,55 @@
+using DAL.Interfaces;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace Controllers.V2;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("2.0")]
+
+public class StatisticsController : ControllerBase
+{
+    private readonly ILogger<StatisticsController> _logger;
+    private readonly IGenericMongoRepository<Statistics> _statsRepository;
+    private readonly IStatisticsService _statsService;
+
+    public StatisticsController(
+        ILogger<StatisticsController> logger,
+        IGenericMongoRepository<Statistics> statsRepository,
+        IStatisticsService statsService)
+        {
+            _logger = logger;
+            _statsRepository = statsRepository;
+            _statsService = statsService;
+        }
+    [MapToApiVersion("2.0")]
+    [HttpGet]
+    [Route("GetStatisticsForPeriod/{userId}/{from}/{to}")]
+
+    public async Task<List<Statistics>> StatisticsForUser(string userId, DateTime from, DateTime to)
+    {
+        var EmptyStats = new Statistics();
+        var result = new List<Statistics> { EmptyStats };
+        return result;
+    }
+
+    [MapToApiVersion("2.0")]
+    [HttpPost]
+    [Route("CreateDailyStatistics/{userId}")]
+    public async Task<IActionResult> CreateDailyStatistics(string userId)
+    {
+        await _statsService.CreateDailyStaticsForUser(userId);
+        return Ok();
+    }
+
+    [MapToApiVersion("2.0")]
+    [HttpGet]
+    [Route("GetLatestStatistics/{userId}")]
+    public async Task<Statistics> GetLatestStatistics(string userId)
+    {
+        var EmptyStats = new Statistics();
+        return EmptyStats;
+    }
+}
