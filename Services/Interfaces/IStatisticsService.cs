2296ab8daadbae8cff44410a46f162b6f343aec6
Frans Billqvist <frans.billquist@gmail.com>
2024-02-19 03:05:20 +0100

    Update IStatisticsService interface

--- Services/Interfaces/IStatisticsService.cs
+++ Services/Interfaces/IStatisticsService.cs
@@ -4,7 +4,9 @@ namespace Services.Interfaces;
 public interface IStatisticsService
 {
     Task CreateDailyStaticsForUser(string userId);
-    Task<List<Statistic>> GetStaticsForPeriod(string userId, DateTime from, DateTime To);
+    Task<Statistic> GetStaticsForPeriod(string userId, DateTime from, DateTime To);
 
     Task<Statistic> GetTemporaryStatisticsOfToday(string userId);
+
+    Task<double> DailyRateStatitics(int used, int limit);
 }
