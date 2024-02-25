ea814d873a9d7bee09c59c8c5916e3252f991beb
Frans Billqvist <frans.billquist@gmail.com>
2024-02-26 00:25:26 +0100

    Add signature to create daily statistics for all users

--- Services/Interfaces/IStatisticsService.cs
+++ Services/Interfaces/IStatisticsService.cs
@@ -4,6 +4,7 @@ namespace Services.Interfaces;
 public interface IStatisticsService
 {
     Task CreateDailyStaticsForUser(string userId);
+    Task CreateDailyStaticsForAllUsers();
     Task<Statistic> GetStaticsForPeriod(string userId, DateTime from, DateTime To);
 
     Task<Statistic> GetTemporaryStatisticsOfToday(string userId);
