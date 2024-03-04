d1108d7086368e4741986d5bdb720877ee3e9a34
Frans Billqvist <frans.billquist@gmail.com>
2024-03-04 04:02:22 +0100

    Add SnuffInfo property to Snuff class

--- DAL/Snuff.cs
+++ DAL/Snuff.cs
@@ -9,5 +9,6 @@ public class Snuff : Document
     public int DefaultAmount { get; set; }
     public string ImageUrl { get; set; }
     public int Size { get; set; }
+    public SnuffInfo? SnuffInfo { get; set; }
 
 }
